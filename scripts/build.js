59ca7ae7269e15a2765b5bcc4a80aa2289ac4cdf
Simon Siefke <simon.siefke@gmail.com>
2023-09-02 15:19:18 +0200

    feature: add build package (#13)

--- scripts/build.js
+++ scripts/build.js
@@ -1,14 +1,1 @@
-import fs from 'node:fs'
-import path, { dirname, join } from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const root = path.join(__dirname, '..')
-
-const dist = path.join(root, 'dist')
-
-fs.rmSync(dist, { recursive: true, force: true })
-fs.mkdirSync(dist, { recursive: true })
-fs.cpSync(join(root, 'static'), dist, { recursive: true, force: true })
-fs.cpSync(join(root, 'packages', 'renderer-worker', 'src'), join(dist, 'packages', 'renderer-worker', 'src'), { recursive: true, force: true })
-fs.cpSync(join(root, 'packages', 'renderer-process', 'src'), join(dist, 'packages', 'renderer-process', 'src'), { recursive: true, force: true })
+import '../packages/build/src/build.js'
