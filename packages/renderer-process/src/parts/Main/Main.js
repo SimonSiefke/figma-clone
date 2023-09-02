32adcba24e26119eb6f3687426c96afa6ef00b2b
Simon Siefke <simon.siefke@gmail.com>
2023-09-02 15:35:27 +0200

    remove console log

--- packages/renderer-process/src/parts/Main/Main.js
+++ packages/renderer-process/src/parts/Main/Main.js
@@ -5,5 +5,4 @@ import * as RendererWorker from '../RendererWorker/RendererWorker.js'
 export const main = async () => {
   CommandState.registerCommands(CommandMap.commandMap)
   await RendererWorker.listen()
-  console.log('created renderer worker')
 }
