10b86589fc7832be1021afc69fec3d64f338fca3
Simon Siefke <simon.siefke@gmail.com>
2023-09-02 13:57:39 +0200

    feature: add favicon

--- scripts/build.js
+++ scripts/build.js
@@ -9,6 +9,6 @@ const dist = path.join(root, 'dist')
 
 fs.rmSync(dist, { recursive: true, force: true })
 fs.mkdirSync(dist, { recursive: true })
-fs.cpSync(join(root, 'index.html'), join(dist, 'index.html'))
+fs.cpSync(join(root, 'static'), dist, { recursive: true, force: true })
 fs.cpSync(join(root, 'packages', 'renderer-worker', 'src'), join(dist, 'packages', 'renderere-worker', 'src'), { recursive: true, force: true })
 fs.cpSync(join(root, 'packages', 'renderer-process', 'src'), join(dist, 'packages', 'renderere-process', 'src'), { recursive: true, force: true })
