0b88ddb890f764ce26c46ce74ca4aa7e5694d591
Simon Siefke <simon.siefke@gmail.com>
2023-09-02 15:29:37 +0200

    fix: cyclic dependencies & bundle code with rollup

--- packages/renderer-worker/src/parts/Main/Main.js
+++ packages/renderer-worker/src/parts/Main/Main.js
@@ -1,7 +1,10 @@
 import * as RemoteCanvas from '../RemoteCanvas/RemoteCanvas.js'
 import * as RendererProcess from '../RendererProcess/RendererProcess.js'
+import * as CommandMap from '../CommandMap/CommandMap.js'
+import * as CommandState from '../CommandState/CommandState.js'
 
 export const main = async () => {
+  CommandState.registerCommands(CommandMap.commandMap)
   await RendererProcess.listen()
   const canvas = await RemoteCanvas.create()
 
