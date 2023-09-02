9204ec700d60b4d78db04cf80439d58de71a20c2
Simon Siefke <simon.siefke@gmail.com>
2023-09-02 13:46:39 +0200

    feature: add dev server

---
+++ packages/server/src/server.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import path, { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const root = path.join(__dirname, '..', '..', '..')
+
+const app = express()
+
+app.use(express.static(root))
+
+console.log({ root })
+
+const handleListening = () => {
+  console.log(`[server] listening on http://localhost:3000`)
+}
+
+const main = () => {
+  app.listen(3000, handleListening)
+}
+
+main()
