36f006ee58907029a6f9ad76e0656c62d1513a5f
Simon Siefke <simon.siefke@gmail.com>
2023-09-02 13:36:51 +0200

    add github actions

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Figma Clone
 
 Just a demo to try out some things with offscreen canvas, webworkers and design perhaps similar to figma.
+
+## Get Started
+
+```sh
+git clone git@github.com:SimonSiefke/figma-clone.git &&
+cd figma-clone &&
+npm ci &&
+npm run dev
+```
---
+++ scripts/update-dependencies.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e
+
+cd $(dirname "$0")
+cd ..
+
+command_exists(){
+  command -v "$1" &> /dev/null
+}
+
+if ! command_exists "ncu"; then
+    echo "npm-check-updates is not installed"
+    npm i -g npm-check-updates
+else
+    echo "ncu is installed"
+fi
+
+function updateDependencies {
+  echo "updating dependencies..."
+  OUTPUT=`ncu -u -x msw -x clean-stack -x prettier -x @types/node`
+  SUB='All dependencies match the latest package versions'
+  if [[ "$OUTPUT" == *"$SUB"* ]]; then
+    echo "$OUTPUT"
+  else
+    rm -rf node_modules package-lock.json dist
+    npm install
+  fi
+}
+
+                                                       updateDependencies             &&
+cd packages/extension-host                          && updateDependencies && cd ../.. &&
+cd packages/main-process                            && updateDependencies && cd ../.. &&
+cd packages/pty-host                                && updateDependencies && cd ../.. &&
+cd packages/renderer-process                        && updateDependencies && cd ../.. &&
+cd packages/extension-host-helper-process           && updateDependencies && cd ../.. &&
+cd packages/renderer-worker                         && updateDependencies && cd ../.. &&
+cd packages/shared-process                          && updateDependencies && cd ../.. &&
+cd packages/server                                  && updateDependencies && cd ../.. &&
+cd packages/e2e                                     && updateDependencies && cd ../.. &&
+cd packages/extension-host-worker-tests             && updateDependencies && cd ../.. &&
+
+cd build                                            && updateDependencies && cd ..    &&
+
+echo "Great Success!"
+
+sleep 2
