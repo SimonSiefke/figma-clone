e408ca9fe2bf95853900e3cfddc7660b6ac82e82
Simon Siefke <simon.siefke@gmail.com>
2023-09-02 16:11:13 +0200

    feature: add more objects (#23)

--- packages/renderer-worker/src/parts/Objects/Objects.js
+++ packages/renderer-worker/src/parts/Objects/Objects.js
@@ -7,4 +7,12 @@ export const objects = [
     height: 100,
     background: 'orange',
   },
+  {
+    type: 'rectangle',
+    x: 50,
+    y: 50,
+    width: 100,
+    height: 100,
+    background: 'red',
+  },
 ]
