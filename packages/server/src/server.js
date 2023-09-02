51527e76ab2854a5ee2b90db2fb408132de2e7ad
Simon Siefke <simon.siefke@gmail.com>
2023-09-02 13:59:52 +0200

    feature: add favicon (#5)

--- packages/server/src/server.js
+++ packages/server/src/server.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import path, { dirname } from 'node:path'
+import path, { dirname, join } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
@@ -8,6 +8,7 @@ const root = path.join(__dirname, '..', '..', '..')
 const app = express()
 
 app.use(express.static(root))
+app.use(express.static(join(root, 'static')))
 
 const handleListening = () => {
   console.log(`[server] listening on http://localhost:3000`)
