b322cd25d88df6f643278df2f2cb867cefd3d903
Simon Siefke <simon.siefke@gmail.com>
2023-09-02 16:05:00 +0200

    feature: make drawing objects more dynamic (#21)

--- packages/renderer-worker/src/parts/Main/Main.js
+++ packages/renderer-worker/src/parts/Main/Main.js
@@ -2,13 +2,11 @@ import * as RemoteCanvas from '../RemoteCanvas/RemoteCanvas.js'
 import * as RendererProcess from '../RendererProcess/RendererProcess.js'
 import * as CommandMap from '../CommandMap/CommandMap.js'
 import * as CommandState from '../CommandState/CommandState.js'
+import * as HandleCanvas from '../HandleCanvas/HandleCanvas.js'
 
 export const main = async () => {
   CommandState.registerCommands(CommandMap.commandMap)
   await RendererProcess.listen()
   const canvas = await RemoteCanvas.create()
-
-  const ctx = canvas.getContext('2d')
-  ctx.fillStyle = 'orange'
-  ctx.fillRect(0, 0, 100, 100)
+  HandleCanvas.handleCanvas(canvas)
 }
