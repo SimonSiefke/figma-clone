3376500eaef3efd3ac6f0ffcf1eb22a08ba47249
Simon Siefke <simon.siefke@gmail.com>
2023-09-03 09:55:08 +0200

    feature: clear old objects when drawing (#27)

--- packages/renderer-worker/src/parts/DrawObjects/DrawObjects.js
+++ packages/renderer-worker/src/parts/DrawObjects/DrawObjects.js
@@ -1,6 +1,7 @@
 import * as DrawObject from '../DrawObject/DrawObject.js'
 
 export const drawObjects = (ctx, objects) => {
+  ctx.clearRect(0, 0, 800, 500)
   for (const object of objects) {
     DrawObject.drawObject(ctx, object)
   }
