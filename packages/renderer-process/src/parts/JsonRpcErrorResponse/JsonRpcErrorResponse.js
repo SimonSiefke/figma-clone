5be3082f735f3c3e242f0919e94a8ac7b2cd0eb9
Simon Siefke <simon.siefke@gmail.com>
2024-11-03 18:06:45 +0100

    fix: types

--- packages/renderer-process/src/parts/JsonRpcErrorResponse/JsonRpcErrorResponse.js
+++ packages/renderer-process/src/parts/JsonRpcErrorResponse/JsonRpcErrorResponse.js
@@ -2,6 +2,7 @@ import * as JsonRpcVersion from '../JsonRpcVersion/JsonRpcVersion.js'
 
 export const create = (message, error) => {
   return {
+    // @ts-ignore
     jsonrpc: JsonRpcVersion.Two,
     id: message.id,
     error,
